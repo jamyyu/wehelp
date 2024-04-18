dfa1e755801cd36d8b616dba65ba31d36e3a5840
jamyyu <103821947+jamyyu@users.noreply.github.com>
2024-04-18 21:59:58 +0800

    Update task3.js

--- week3/task3.js
+++ week3/task3.js
@@ -19,7 +19,7 @@ window.addEventListener('resize', function(){
 fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1").then(function(response){
     return response.json();
 }).then(function(data){
-    let imgList=[]
+    let imgList=[];
     let spotList=data.data.results;
     //console.log(spotList)
     let spotList3=spotList.slice(0,13);
